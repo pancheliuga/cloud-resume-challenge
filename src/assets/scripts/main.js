2101cda5143588876c3422ca46ffe1dd12e3691b
pancheliuga <pan4ik@gmail.com>
2023-07-04 14:52:32 +0300

    add function for fetching API Gateway

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,6 +2,30 @@ import 'focus-visible'
 
 document.documentElement.classList.remove('no-js')
 
+const API = 'https://nbh86frdn8.execute-api.eu-central-1.amazonaws.com'
+
+//Get Views
+async function getViews() {
+    try {
+        let response = await fetch(API)
+        let data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Render Views
+async function renderViews() {
+    const views = await getViews()
+    if (views) {
+        const viewsContainer = document.querySelector('#views')
+        viewsContainer.innerHTML = views
+    }
+}
+
+renderViews()
+
 // Scroll State
 const onScroll = () => {
     const scrollClassName = 'js-scrolled'
